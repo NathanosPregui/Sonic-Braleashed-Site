4ead419231bc4a19ce4ff1b8cd42eb67c7299733
NathanosPregui <salesnathan188@gmail.com>
2025-06-02 15:53:15 -0300

    Trocando o BrowserRoute para hashrouter, funcionamento melhor no gitpages

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,19 @@ import Footer from "./components/footer.jsx";
 import Home from "./pages/Home.jsx";
 import Downloads from "./pages/Downloads.jsx";
 import { Sobre } from "./pages/Sobre.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename='Sonic-Braleashed-Site'>
+    <HashRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/Downloads' element={<Downloads />} />
-        <Route path='/Sobre' element={<Sobre />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Downloads" element={<Downloads />} />
+        <Route path="/Sobre" element={<Sobre />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
